7ca3052091bc6aab02bc552ae6c45d3ba78417dd
davidrpiper <drppublic@gmail.com>
2015-03-12 10:50:05 +1030

    dnsenum head only

--- dnsenum.rb
+++ dnsenum.rb
@@ -3,9 +3,17 @@ class Dnsenum < Formula
   head "https://github.com/fwaeytens/dnsenum.git"
 
   def install
-    bin.install "dnsenum.pl"
-    (prefix/"bin").install "dns.txt"
+    system "chmod +x dnsenum.pl"
+    (libexec/"bin").install "dnsenum.pl"
+    (libexec/"bin").install "dns.txt"
     prefix.install "README.md"
     prefix.install "INSTALL.txt"
+    bin.write_exec_script libexec/"bin/dnsenum.pl"
+  end
+
+  def caveats; <<-EOS.undent
+    This script requires the Net::Netmask perl module. Install with:
+      cpan -i Net::Netmask
+    EOS
   end
 end

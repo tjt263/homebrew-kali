f3b44273558befa62cf5adaf53731ddeab032a73
davidrpiper <drppublic@gmail.com>
2015-03-12 11:48:17 +1030

    doona head only

--- doona.rb
+++ doona.rb
@@ -1,37 +1,16 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Doona < Formula
-  homepage ""
-  head "git://git.kali.org/packages/doona.git"
-  
-
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
+  homepage "https://github.com/wireghoul/doona"
+  head "https://github.com/wireghoul/doona", :using => :git
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test doona`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    prefix.install "README"
+    prefix.install "Docs"
+    libexec.install "bedmod"
+    system "chmod +x doona.pl"
+    libexec.install "doona.pl"
+    bin.write_exec_script libexec/"doona.pl"
+    inreplace bin/"doona.pl" do |s|
+      s.gsub! /exec "(.+)\/doona.pl"/, 'cd \1; exec "./doona.pl"'
+    end
   end
 end

88120a40eefea8597c639a4111c955d42e9778e3
davidrpiper <drppublic@gmail.com>
2015-03-12 20:11:13 +1030

    automater head only

--- automater.rb
+++ automater.rb
@@ -1,37 +1,22 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Automater < Formula
-  homepage ""
+  homepage "http://www.tekdefense.com/automater/"
   head "git://git.kali.org/packages/automater.git"
-  
-
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
+    prefix.install "docs"
+    prefix.install "README.md"
+    prefix.install "LICENSE"
+    system "chmod +x Automater.py"
+    libexec.install "Automater.py"
+    libexec.install "inputs.py"
+    libexec.install "outputs.py"
+    libexec.install "siteinfo.py"
+    libexec.install "sites.xml"
+    libexec.install "utilities.py"
+    bin.write_exec_script libexec/"Automater.py"
 
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test automater`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    inreplace bin/"Automater.py" do |s|
+      s.gsub! /exec "(.+)\/Automater.py"/, 'cd \1; exec "./Automater.py"'
+    end
   end
 end

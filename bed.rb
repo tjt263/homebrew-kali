2d906ee72c16e712285a52d32f9686d0237d2410
davidrpiper <drppublic@gmail.com>
2015-03-12 11:56:03 +1030

    bed head only

--- bed.rb
+++ bed.rb
@@ -1,37 +1,21 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Bed < Formula
-  homepage ""
+  homepage "http://tools.kali.org/vulnerability-analysis/bed"
   head "git://git.kali.org/packages/bed.git"
   
-
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    prefix.install "README"
+    prefix.install "docs"
+    libexec.install "bedmod"
+    system "chmod +x bed.pl"
+    libexec.install "bed.pl"
+    bin.write_exec_script libexec/"bed.pl"
+    inreplace bin/"bed.pl" do |s| 
+      s.gsub! /exec "(.+)\/bed.pl"/, 'cd \1; exec "./bed.pl"'
+    end
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test bed`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+  def caveats; <<-EOS.undent
+    Prefer doona.pl (provided by this Tap). doona is an updated and maintained fork of bed.
+  EOS
   end
 end

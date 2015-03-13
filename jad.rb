0a569dfb9f763c6b742fd26b3b7e289b8fd8ffd1
davidrpiper <drppublic@gmail.com>
2015-03-13 13:28:54 +1030

    jad complete

--- jad.rb
+++ jad.rb
@@ -1,37 +1,19 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Jad < Formula
-  homepage ""
-  head "git://git.kali.org/packages/jad.git"
+  homepage "https://web.archive.org/web/20080214075546/http://www.kpdus.com/jad.html"
+  url "https://web.archive.org/web/20080214075546/http://www.kpdus.com/jad/macosx86/jadmx158.zip"
+  version "1.5.8"
+  sha1 "25bf73676eec21d4cf02f110cb7a06d37cb52f16"
   
-
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
+    bin.install "jad"
+    man1.install "jad.1"
+    prefix.install "Readme.txt"
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test jad`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+  def caveats; <<-EOS.undent
+    JAD is not maintained, and this 'latest' version was built for OS X 10.4.6 (Intel).
+    The JAD homepage is only available via the Wayback Machine.
+    Prefer jd-gui instead.
+    EOS
   end
 end
